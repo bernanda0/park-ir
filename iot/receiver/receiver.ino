98f0d3a2b7ecdd314e1fe3f465779d6a08a8361c
rayhan-akbar <rayhan.akbar@ui.ac.id>
2023-12-05 18:07:41 +0700

    update transmitter - Added BLE

--- iot/receiver/receiver.ino
+++ iot/receiver/receiver.ino
@@ -2,17 +2,24 @@
 #include <IRremote.hpp>
 #include <WiFi.h>
 #include <WiFiMulti.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+// #include <freertos/FreeRTOS.h>
+// #include <freertos/task.h>
+#include <ESP32Servo.h>
 
 #define IR_RECEIVE_PIN 25
 #define LED_BUILTIN 2 // Replace with the actual built-in LED pin
 #define MOTOR_PIN 4   // Replace with the actual pin controlling the DC motor
+#define SOUND_SPEED 0.034
 
 const char* ssid = "CogniSafe";
 const char* password = "12345678";
 const char* endpoint = "http://192.168.137.1:4444/plate/verify";
-
+Servo myservo;  // create servo object to control a servo
+int servoPin = 33;
+const int trigPin = 32;
+const int echoPin = 35;
+long duration;
+float distanceCm;
 // ini seharusnya didapat dr transmitter
 String g_receivedID;
 char receivedID[20];
@@ -26,17 +33,20 @@ SemaphoreHandle_t httpSemaphore, receiverSemaphore;
 
 void taskIRReceiver(void *pvParameters) {
   while (1) { 
-    if (IrReceiver.decode() && readAgain) {
-      if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-          Serial.printf("Received noise or an unknown protocol wait 1 seconds %d", counter++);      
-      } else {
-          sprintf(receivedID, "VID%08X", IrReceiver.decodedIRData.decodedRawData);
-          Serial.printf("%s\n", receivedID);
-          xSemaphoreGive(receiverSemaphore); 
+    if(distanceCm <= 5){
+      if (IrReceiver.decode() && readAgain) {
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            Serial.printf("Received noise or an unknown protocol wait 1 seconds %d\n", counter++);      
+        } else {
+            sprintf(receivedID, "VID%08X", IrReceiver.decodedIRData.decodedRawData);
+            Serial.printf("%s\n", receivedID);
+            xSemaphoreGive(receiverSemaphore); 
+        }
+        IrReceiver.resume();
       }
-      IrReceiver.resume();
+    }else{
+      myservo.write(0);    // tell servo to go to position in variable 'pos'
     }
-  
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
@@ -71,27 +81,43 @@ void taskHTTPClient(void *pvParameters) {
          Serial.println("Client failed!");
       }
     }
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
 void onRequestSuccess(void *pvParameters) {
   while (1) {
     if (xSemaphoreTake(httpSemaphore, portMAX_DELAY) == pdTRUE) {
-      Serial.println("OnSuccess wait 5 seconds");
       digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
       vTaskDelay(300 / portTICK_PERIOD_MS); // Keep the LED on for 1 second
       digitalWrite(LED_BUILTIN, LOW); // Turn off the LED
       
       // Control the DC motor to open the gate
-      digitalWrite(MOTOR_PIN, HIGH);
+      //digitalWrite(MOTOR_PIN, HIGH);
+      myservo.write(90);    // tell servo to go to position in variable 'pos'
       vTaskDelay(1000 / portTICK_PERIOD_MS); // Run the motor for 1 second
       digitalWrite(MOTOR_PIN, LOW);
 
-      vTaskDelay(5000);
       readAgain = true;
     }
     vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void vSensorTask(void *pvParam){
+  while(1){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculate the distance
+    distanceCm = duration * SOUND_SPEED/2;
+    vTaskDelay(pdMS_TO_TICKS(100)); //memberikan delay
+
   }
 }
 
@@ -99,7 +125,11 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
-
+  pinMode(IR_RECEIVE_PIN, INPUT);
+  myservo.setPeriodHertz(100);    // standard 50 hz servo
+	myservo.attach(servoPin); 
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP(ssid, password);
@@ -118,6 +148,8 @@ void setup() {
   xTaskCreatePinnedToCore(taskHTTPClient, "HTTPClient", 10000, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(taskIRReceiver, "IRReceiver", 10000, NULL, 1, NULL, 0);
   xTaskCreatePinnedToCore(onRequestSuccess, "RequestSuccessCallback", 10000, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(vSensorTask, "SensorTask", 10000, NULL, 2, NULL, 1);
+
 
 }
 
--- iot/transmitter/transmitter.ino
+++ iot/transmitter/transmitter.ino
@@ -1,31 +1,110 @@
 #include <Arduino.h>
 #include <IRremote.hpp>
 #include "PinDefinitionsAndMore.h"  // Define macros for input and output pin etc.
+#include <BluetoothSerial.h>
 
 #define IR_SEND_PIN         25
 #define IR_SEND_PIN_STRING "25"
 #define DISABLE_CODE_FOR_RECEIVER // Disables restarting receiver after each send. Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not used.
-#define DELAY_AFTER_SEND 3000
+#define DELAY_AFTER_SEND 1000
 
 // this hardcoded, example VID958F6538
-uint32_t plateID = 0x958F6538;
+uint32_t plateID = 0;
+BluetoothSerial SerialBT; //variabel untuk serial monitor BT
+bool BT_isOn = false; //Boolean untuk menandakan BT sudah on atau belum
+bool BT_isConnected = false; //Boolean untuk menandakan BT sudah terhubung atau tidak
+bool btn_toggle = false;
+int btn = 32;
+
+//Class callback yang akan diassign ke SerialBT
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+    if (event == ESP_SPP_SRV_OPEN_EVT) { //Jika tersambung
+        BT_isConnected = true; //mengubah boolean menjadi true
+        Serial.println("Bluetooth tersambung");
+    } else if (event == ESP_SPP_CLOSE_EVT) { //jika terputus
+        BT_isConnected = false; //mengubah boolean menjadi false
+        Serial.println("Bluetooth terputus");
+    }
+}
+
+void vButtonRead(void *pvParam){
+  while(1){
+    if(digitalRead(btn) == LOW){
+      if(btn_toggle == false){
+        btn_toggle = true;
+        Serial.println("toggle on");
+        delay(50);
+      }else{
+        btn_toggle = false;
+        Serial.println("toggle off");
+        delay(50);
+      }
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+  }
+}
+void vBTinit(void *pvParam){
+  while(1){
+    if(btn_toggle == true && BT_isOn == false){
+      //Memulai SerialBT
+      SerialBT.begin("esp2");
+      BT_isOn = true; //mengubah boolean menjadi true
+      Serial.println("Bluetooth telah dimulai");
+    
+    }else if(btn_toggle == false && BT_isOn == true){
+      //SerialBT.end();
+      BT_isOn = false;
+      Serial.println("Bluetooth telah dihentikan");
+
+    }
+     vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void vBTtask(void *pvParam){
+  String command;
+  while(1){
+    if(BT_isConnected == true){
+      vTaskDelay(pdMS_TO_TICKS(50));
+      //Jika SerialBT tidak tersedia
+      if (SerialBT.available() <= 0){
+        continue; //skip program dibawahnya
+      }
+      command = SerialBT.readStringUntil('\n');
+      Serial.println(command);
+      plateID = strtoul(command.c_str(), NULL, 16);
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+  }
+}
 
 void setup() {
     Serial.begin(115200);
+    pinMode(btn, INPUT_PULLUP);
+    SerialBT.register_callback(callback);
 
-#if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
-    delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
-#endif
+  #if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/|| defined(SERIALUSB_PID) || defined(ARDUINO_attiny3217)
+      delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
+  #endif
     // Just to know which program is running on my Arduino
     Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-    IrSender.begin(); // Start with IR_SEND_PIN as send pin and enable feedback LED at default feedback LED pin
+    IrSender.begin(IR_SEND_PIN); // Start with IR_SEND_PIN as send pin and enable feedback LED at default feedback LED pin
     Serial.println(F("Send IR signals at pin " IR_SEND_PIN_STRING));
     Serial.println(F("Send IR signals at pin " STR(IR_SEND_PIN)));
+
+    xTaskCreatePinnedToCore(vBTinit, "BT init", 2048, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(vButtonRead, "button read", 2048, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(vBTtask, "SerialBT read", 2048, NULL, 1, NULL, 0);
 }
 
 
 void loop() {
-    Serial.printf("Send NECRaw(%s)\n", plateID);
+  if(plateID != 0){
+    Serial.print("Sending data: ");
+    Serial.printf("%x\n",plateID);
     IrSender.sendNECRaw(plateID, 0);
     delay(DELAY_AFTER_SEND);
+  }
 }
